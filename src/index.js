727fb854496a1b86cb1cab89e6378311a543256a
Yaseen <mhdyaz@outlook.com>
2021-04-30 12:48:17 +0300

    Added Home Component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Home from './screens/home/Home.js';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <div />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
